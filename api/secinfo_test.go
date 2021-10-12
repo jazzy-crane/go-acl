95136019939cb505214d8cbb58b49c2524fb4c36
Alex Crane <crane@ava.uk>
2021-10-12 14:10:39 +0100

    Add API for setting ACLs on any Windows objects via GetSecurityInfo/SetSecurityInfo

--- api/secinfo_test.go
+++ api/secinfo_test.go
@@ -15,7 +15,10 @@ func TestGetNamedSecurityInfo(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove(f.Name())
+	defer func() {
+		_ = f.Close()
+		_ = os.Remove(f.Name())
+	}()
 	var (
 		secDesc windows.Handle
 	)
@@ -39,9 +42,72 @@ func TestSetNamedSecurityInfo(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove(f.Name())
+	defer func() {
+		_ = f.Close()
+		_ = os.Remove(f.Name())
+	}()
 	if err = SetNamedSecurityInfo(
 		f.Name(),
+		SE_FILE_OBJECT,
+		DACL_SECURITY_INFORMATION,
+		nil,
+		nil,
+		0,
+		0,
+	); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetSecurityInfo(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = f.Close()
+		_ = os.Remove(f.Name())
+	}()
+	var secDesc windows.Handle
+
+	if err = GetSecurityInfo(
+		windows.Handle(f.Fd()),
+		SE_FILE_OBJECT,
+		0,
+		nil,
+		nil,
+		nil,
+		nil,
+		&secDesc,
+	); err != nil {
+		t.Fatal(err)
+	}
+	defer windows.LocalFree(secDesc)
+}
+
+func TestSetSecurityInfo(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	fName16, err := windows.UTF16PtrFromString(f.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fHandle, err := windows.CreateFile(fName16, windows.GENERIC_ALL, 0, nil, windows.OPEN_EXISTING, 0, windows.InvalidHandle)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer windows.CloseHandle(fHandle)
+
+	if err = SetSecurityInfo(
+		fHandle,
 		SE_FILE_OBJECT,
 		DACL_SECURITY_INFORMATION,
 		nil,
--- apply.go
+++ apply.go
@@ -53,3 +53,48 @@ func Apply(name string, replace, inherit bool, entries ...api.ExplicitAccess) er
 		0,
 	)
 }
+
+// ApplyHandle the provided access control entries to a file. If the replace
+// parameter is true, existing entries will be overwritten. If the inherit
+// parameter is true, the file will inherit ACEs from its parent.
+func ApplyHandle(handle windows.Handle, objectType int32, replace, inherit bool, entries ...api.ExplicitAccess) error {
+	var oldAcl windows.Handle
+	if !replace {
+		var secDesc windows.Handle
+		api.GetSecurityInfo(
+			handle,
+			objectType,
+			api.DACL_SECURITY_INFORMATION,
+			nil,
+			nil,
+			&oldAcl,
+			nil,
+			&secDesc,
+		)
+		defer windows.LocalFree(secDesc)
+	}
+	var acl windows.Handle
+	if err := api.SetEntriesInAcl(
+		entries,
+		oldAcl,
+		&acl,
+	); err != nil {
+		return err
+	}
+	defer windows.LocalFree((windows.Handle)(unsafe.Pointer(acl)))
+	var secInfo uint32
+	if !inherit {
+		secInfo = api.PROTECTED_DACL_SECURITY_INFORMATION
+	} else {
+		secInfo = api.UNPROTECTED_DACL_SECURITY_INFORMATION
+	}
+	return api.SetSecurityInfo(
+		handle,
+		objectType,
+		api.DACL_SECURITY_INFORMATION|secInfo,
+		nil,
+		nil,
+		acl,
+		0,
+	)
+}
